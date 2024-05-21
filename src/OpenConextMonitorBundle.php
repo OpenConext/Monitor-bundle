a72ca0ddb68c97e95ab477bce02b757493537e9c
Paul Rijke <paul@ibuildings.nl>
2024-05-21 19:33:23 +0200

    Refactor services to new filestructure

--- src/OpenConextMonitorBundle.php
+++ src/OpenConextMonitorBundle.php
@@ -26,6 +26,6 @@ class OpenConextMonitorBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $container->import('../config/services.yml');
+        $container->import('../config/services.yaml');
     }
 }
